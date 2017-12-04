36d522a354ef344a1f124e2dd499427abed6d975
csielee <lee097019@gmail.com>
2017-12-05 01:38:35 +0800

    Add file:// will not jump to luffy

--- js/jumpToLuffy.js
+++ js/jumpToLuffy.js
@@ -1,7 +1,7 @@
 
 console.log('this is domain : '+document.domain);
 
-if (document.domain != 'luffy.ee.ncku.edu.tw') {
+if (window.location.search == '' && window.location.origin != 'file://' && document.domain != 'luffy.ee.ncku.edu.tw') {
     var newUrl = 'https://luffy.ee.ncku.edu.tw/~csielee'+document.location.pathname;
     console.log('we will goto ' + newUrl);
     window.location.replace(newUrl);
