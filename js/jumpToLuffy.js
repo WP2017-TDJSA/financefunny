bcb267a8615b58152f68f999262b88cd79af5cde
csielee <lee097019@gmail.com>
2017-12-05 01:22:56 +0800

    Add jumpToLuffy function

--- js/jumpToLuffy.js
+++ js/jumpToLuffy.js
@@ -1,2 +1,8 @@
 
 console.log('this is domain : '+document.domain);
+
+if (document.domain != 'luffy.ee.ncku.edu.tw') {
+    var newUrl = 'https://luffy.ee.ncku.edu.tw/~csielee'+document.location.pathname;
+    console.log('we will goto ' + newUrl);
+    window.location.replace(newUrl);
+}
