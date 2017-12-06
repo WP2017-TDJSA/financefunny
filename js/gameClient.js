ab984d809704378f09fa90bf83abe4bc1df94809
csielee <lee097019@gmail.com>
2017-12-06 12:28:08 +0800

    Add hall namespace by socket.io

--- js/gameClient.js
+++ js/gameClient.js
@@ -4,23 +4,40 @@ const gameServerURL = 'http://127.0.0.1:8787';
 
 var gameClient = (server = gameServerURL) => {
     var _this = {};
-    _this.server = server;
+    _this.serverURL = server;
     _this.connectGameServer = connectGameServer;
+    _this.connectOK = false;
+    _this.enterHall = enterHall;
 
     return _this;
 }
 
-function connectGameServer() {
-    if (this.hasOwnProperty('socket'))
-        return this.socket;
+function connectGameServer(status) {
+    if (this.hasOwnProperty('server'))
+        return this.server;
 
     try {
-        this.socket = io(this.server);    
+        this.server = io(this.serverURL);    
     } catch (error) {
         console.log('no include socket.io client js');
         return null;
     }
-    this;
+    this.server.on('status', (ack) => {
+        ack(status);
+        this.connectOK = true;
+    });
 
-    return this.socket;
+    return this.server;
+}
+
+function enterHall(callback) {
+    if (this.connectOK == false)
+        return;
+
+    if (!this.hasOwnProperty('hall'))
+        this.hall = io(this.serverURL + '/hall');
+    
+    this.hall.on('roomList',(data) => {
+        callback(data.roomList)
+    })
 }
--- server/gameServer.js
+++ server/gameServer.js
@@ -2,8 +2,6 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const CollectionAuction = require("../js/CollectionAuction.js");
-const homeRoom = 'home'
-var roomList = [homeRoom];
 
 app.get('/', function(req, res){
 	//res.sendfile('index.html');
@@ -13,31 +11,174 @@ app.get('/', function(req, res){
 
 function disconnectHandler(socket) {
 	return (reason) => {
-		if (socket.hasOwnProperty('room')) {
+		/*if (socket.hasOwnProperty('room')) {
 			socket.broadcast.to(socket.room).emit('deleate user');
 			socket.leave(socket.room);
-		}
-
-		console.log('a user disconnected' + reason);
+		}*/
+		delete io.playerList[getID(socket.id)];
+		console.log('a user disconnected\n' + reason);
 	}
 }
 
 function playerStatusHandler(socket) {
 	return (userdata) => {
-		if (roomList.indexOf(userdata.status)) {
+		/*if (roomList.indexOf(userdata.status)) {
 			socket.room = userdata.status;
 			socket.join(socket.room);
 			socket.broadcast.to(socket.room).emit('new user',userdata);
-		}
+		}*/
 	};		
 }
 
+function getID(id) {
+	if (id.indexOf('#') == -1)
+		return id;
+	return id.substring(id.indexOf('#'), id.length)
+}
+
+io.playerList = {};
+
 io.on('connection', function(socket){
-	console.log('a user connected');
-	socket.on('player status', playerStatusHandler());
+	console.log('a user connected : '+socket.id);
+	// get player status
+	socket.emit('status', (data)=>{
+		data.id = getID(socket.id);
+		console.log('<'+data.id+'> '+JSON.stringify(data));
+		io.playerList[getID(socket.id)] = data;
+	});
+
+	//socket.on('player status', playerStatusHandler());
 	socket.on('disconnect', disconnectHandler);
 	//socket.join(homeRoom);
 	//socket.broadcast.to(homeRoom).emit('new user',{ a : 'a'});
+});
+
+var hall = io.of('/hall');
+hall.roomList = [];
+var roomInfo = (roomName='',maxPlayers=3) => {
+	var _this = {
+		maxPlayers : maxPlayers,
+		currentPlayers : 0,
+		roomName : roomName,
+		playerList : [],
+		join : (id)=>{
+			_this.currentPlayers++
+			_this.playerList.push(io.playerList[id]);
+			// update
+			hall.to(_this.roomName).emit('roomInfo',_this);
+			hall.emit('roomList',{ roomList : hall.roomList});
+		},
+		leave : (id)=>{
+			_this.currentPlayers--
+			_this.playerList.splice(hall.roomList.indexOf(io.playerList[id]),1); 
+			// remove room
+			if (_this.currentPlayers == 0) {
+				if (hall.roomList.indexOf(_this) >= 0) {
+					hall.roomList.splice(hall.roomList.indexOf(_this),1);
+				}
+			}
+			// update
+			hall.to(_this.roomName).emit('roomInfo',_this);
+			hall.emit('roomList',{ roomList : hall.roomList});
+		}
+	}
+	return _this;
+}
+
+hall.on('connection', function(socket) {
+	console.log('a user enter hall : ' + socket.id);
+	// send room info
+	socket.emit('roomList',{ roomList : hall.roomList});
+	// listen create room
+	socket.on('createRoom',(data, ack) => {
+		console.log(socket.rooms);
+		// check not in other room
+		if (socket.rooms.length != 1) {
+			ack('你已經在其他房間！')
+			return;
+		}
+		if (typeof data != 'string') {
+			ack('資料格式錯誤')
+			return;
+		}
+		// create room
+		var newRoom = roomInfo(data);
+		hall.roomList.push(newRoom);
+		// self join room
+		socket.join(newRoom.roomName,() => {
+			newRoom.join(getID(socket.id));
+			ack(newRoom);
+		});
+	})
+	// enter room
+	socket.on('enterRoom',(data, ack) => {
+		// check not in other room
+		if (socket.rooms.length != 1) {
+			ack('你已經在其他房間！')
+			return;
+		}
+		if (typeof data != 'string') {
+			ack('資料格式錯誤')
+			return;
+		}
+		// find room
+		var chooseRoom = null;
+		hall.roomList.forEach(element => {
+			if (element.roomName == data)
+				chooseRoom = element;	
+		});
+		if (chooseRoom == null) {
+			ack('沒有發現房間')
+			return;
+		}
+		// enter room
+		socket.join(chooseRoom.roomName,() => {
+			chooseRoom.join(getID(socket.id));
+			ack(chooseRoom);
+		});
+	})
+	// leave room
+	socket.on('leaveRoom',(data) => {
+		if (typeof data != 'string') {
+			ack('資料格式錯誤')
+			return;
+		}
+		// find room
+		var chooseRoom = null;
+		hall.roomList.forEach(element => {
+			if (element.roomName == data)
+				chooseRoom = element;	
+		});
+		if (chooseRoom == null) {
+			ack('沒有發現房間')
+			return;
+		}
+		// leave room
+		socket.leave(chooseRoom.roomName,() => {
+			chooseRoom.leave(getID(socket.id));
+			ack(chooseRoom);
+		});
+	})
+	socket.on('disconnecting', (reason) => {
+		if (socket.rooms.length > 1) {
+			var data = socket.rooms[1];
+			var chooseRoom = null;
+			hall.roomList.forEach(element => {
+				if (element.roomName == data)
+					chooseRoom = element;	
+			});
+			if (chooseRoom != null) {
+				// leave room
+				socket.leave(chooseRoom.roomName,() => {
+					chooseRoom.leave(getID(socket.id));
+				});
+			}
+		}
+	})
+
+	socket.on('disconnect',()=>{
+		console.log('a user leave hall');
+	});
 });
 
 http.listen(8787,  function(){
