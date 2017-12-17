7057456988e886540669eea98e28f28c0ff4eb5d
csielee <lee097019@gmail.com>
2017-12-17 21:08:20 +0800

    Fix some bug

--- gameApp/gameApp.js
+++ gameApp/gameApp.js
@@ -6,33 +6,24 @@ import Phaser from 'phaser'
 
 import player from './player'
 
-console.log($);
-
-console.log(Phaser.Game);
-
-console.log(player);
-
-console.log('hi');
-
 var targetWidth;  
 var targetHeight;
 var game
 var start = {
     preload : function() {
-
+        game.load.image('a', 'img/game/background.jpg');
+        game.load.image('b', 'img/game/black.png');
     },
     create : function() {
-
+        console.log('hi i am create')
+        game.add.sprite(0,0,'a');
     }
 }
 
 $(document).ready(()=>{
-    window.PIXI = PIXI
-    window.p2 = p2
-    window.Phaser = Phaser
-    targetHeight =  window.innerHeight;
-    targetWidth = window.innerWidth;
-    console.log(Phaser.Game);
+    targetHeight =  800;
+    targetWidth = 600;
+    
     game = new Phaser.Game(targetWidth, targetHeight, Phaser.CANVAS, 'gameDiv',null);
     game.resolution=window.devicePixelRatio;
     game.state.add('start', start);
