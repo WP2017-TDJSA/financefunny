8efd221f1f0a446a5ab5216b9ccd60d51f9d5e93
csielee <lee097019@gmail.com>
2017-12-17 23:48:32 +0800

    Add collectionauction logicl

--- gameApp/defaultState.js
+++ gameApp/defaultState.js
@@ -1,13 +1,13 @@
-
 module.exports = function(game) {
     return {
         preload : function() {
+            console.log('[state] default')
             game.load.image('a', 'img/game/background.jpg');
             game.load.image('b', 'img/game/black.png');
         },
         create : function() {
-            console.log('hi i am create')
-            game.add.sprite(0,0,'a');
+            
+            game.add.sprite(0,0,'b');
         }
     };
 }
--- gameApp/gameApp.js
+++ gameApp/gameApp.js
@@ -4,19 +4,53 @@ import 'pixi'
 import 'p2'
 import Phaser from 'phaser'
 
-import player from './player'
-import defaultState from './defaultState'
-
 var targetWidth;  
 var targetHeight;
-var game
+var game;
 
 $(document).ready(()=>{
     targetHeight =  800;
     targetWidth = 600;
-    
+    // 創造 phaser 遊戲
     game = new Phaser.Game(targetWidth, targetHeight, Phaser.CANVAS, 'gameDiv',null);
     game.resolution=window.devicePixelRatio;
-    game.state.add('default', defaultState(game));
+
+    // 加入遊戲狀態
+    game.state.add('default', require('./defaultState')(game));
+    game.state.add('auction', require('./auctionState')(game));
+
+    // 開始進行遊戲狀態
     game.state.start('default');
+})
+
+// 抓取左右方向鍵，切換 state
+// 37 左 39 右
+$(document).keydown((event)=>{
+    
+    switch(event.which) {
+        case 37 :
+            if (game) {
+                var currState = game.state.current;
+                var index = Object.keys(game.state.states).indexOf(currState);
+                if (index == 0)
+                    index = Object.keys(game.state.states).length - 1;
+                else
+                    index--;
+                
+                game.state.start(Object.keys(game.state.states)[index]);
+            }
+            break;
+        case 39 :
+            if (game) {
+                var currState = game.state.current;
+                var index = Object.keys(game.state.states).indexOf(currState);
+                if (index == Object.keys(game.state.states).length - 1)
+                    index = 0;
+                else
+                    index++;
+            
+                game.state.start(Object.keys(game.state.states)[index]);
+            }
+            break;
+    }
 })
