21549a7761faa433d6f524a38ac622d678ea4a38
e24033150 <ann750028@gmail.com>
2017-12-20 18:27:58 +0800

    two people walk

--- gameApp/walk.js
+++ gameApp/walk.js
@@ -1,5 +1,6 @@
 module.exports = function(game) {
-	var walk;
+	var richwalk;
+	var sanhuwalk;
 	return {
         
 		preload : function() {
@@ -9,29 +10,39 @@ module.exports = function(game) {
             var img = game.add.sprite(0,0,'a');
 			img.width = game.width*window.devicePixelRatio;
 			img.height = game.height*window.devicePixelRatio;
-			walk = game.add.sprite(game.width, game.height/3, 'walk');
+			richwalk = game.add.sprite(game.width, game.height/3, 'richwalk');
+			sanhuwalk = game.add.sprite(0, game.height/3, 'sanhuwalk');
 			
-			var width = walk.width;
-			var height = walk.height;
-			//walk.scale.setTo(0.5,0.5);
-			//walk.scale.x = -0.5;
-			walk.width = -game.width*0.3*window.devicePixelRatio;
-			walk.height = -walk.width*height/width;
+			var width = richwalk.width;
+			var height = richwalk.height;
 			
-			walk.animations.add('walk', Phaser.Animation.generateFrameNames('walk', [1,2,3,4,5,6,7,8]), 4, true);
-			walk.animations.play('walk');
+			richwalk.width = -game.width*0.3*window.devicePixelRatio;
+			richwalk.height = -richwalk.width*height/width;
+			sanhuwalk.width = game.width*0.3*window.devicePixelRatio;
+			sanhuwalk.height = sanhuwalk.width*height/width;
+			
+			richwalk.animations.add('richwalk',[ 0,1,2,3,4,5,6,7,8], 8, true,true);
+			richwalk.animations.play('richwalk');
+			sanhuwalk.animations.add('sanhuwalk',[ 0,1,2,3,4,5,6,7,8], 8, true,true);
+			sanhuwalk.animations.play('sanhuwalk');
+			
         },
 		
 		update : function(){
 			
-			if(walk.x > (window.innerWidth*6/7))
+			if(richwalk.x > (window.innerWidth*0.9))
 			{
-				walk.x -= 1;
+				richwalk.x -= 1;
+				sanhuwalk.x += 1;
 			}
-			if(walk.x <= (window.innerWidth*6/7))
+			if(richwalk.x <= (window.innerWidth*0.9))
 			{
-				walk.animations.stop(null, true);
+				richwalk.animations.stop(null, true);
+				richwalk.frame = 9;
+				sanhuwalk.animations.stop(null, true);
+				sanhuwalk.frame = 9;
 			}
+			
 			
 			
 		},
