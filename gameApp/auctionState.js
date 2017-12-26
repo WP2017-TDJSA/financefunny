ee2347982741e79283778ec8f606fc266a73ddd4
csielee <lee097019@gmail.com>
2017-12-26 21:32:34 +0800

    Modify layout

--- gameApp/auctionState.js
+++ gameApp/auctionState.js
@@ -1,16 +1,20 @@
 function cell(game, x, y, width, height) {
     var graphics = game.add.graphics(0, 0);
-    graphics.lineStyle(1,0xffffff,1);
-    graphics.beginFill(0xffffff,0.3);
-    graphics.drawRoundedRect(0,0,width,height,5);
+    graphics.lineStyle(3,0x5aedb9,1);
+    graphics.beginFill(0xffffff,1);
+    //graphics.drawRoundedRect(0,0,width,height,5);
+    graphics.drawEllipse(0,0,width/2,height/2)
     graphics.endFill();
     var sprite = game.add.sprite(x, y, graphics.generateTexture())
     //sprite.anchor.set(0.5, 0.5)
     //graphics.visible = false;
     graphics.destroy();
-    var style = { font: "18px 微軟正黑體", fill: "#ffffff",  align: "center"}
-    var text = game.add.text(width/2, height*0.6, "", style);
-    text.anchor.set(0.5, 0.5);
+    var style = { font: "18px 微軟正黑體", fill: "#000000",  align: "center", boundsAlignH:"center", boundsAlignV:"middle"}
+    var text = game.add.text(0, 0, "", style);
+    text.setTextBounds(0,0,width,height)
+    //text.anchor.set(0.5, 0.5);
+    
+    //console.log(text.textBounds)
     sprite.addChild(text);
     sprite.text = text;
     return sprite;
@@ -64,6 +68,104 @@ function table(game, x, y, innerHeight,cellwidth, cellheight, maxCount) {
     return sprite;
 }
 
+function auctionMachine(game, x, y, width, height) {
+    // create sprite
+    var sprite = game.add.sprite(x,y)
+    
+    // create title
+    sprite.title = [];
+    var style = { font: "24px 微軟正黑體", fill: "#5aedb9",  align: "center", boundsAlignH:"center", boundsAlignV:"middle"}
+    for (var i=0;i<3;i++) {
+        var text = game.add.text(0,0,"",style);
+        //text.anchor.set(0.5, 0.5);
+        sprite.addChild(text);
+        sprite.title.push(text);
+    }
+    sprite.setTitle = (arr) => {
+        for (var i=0;i<3;i++) {
+            sprite.title[i].setText(arr[i])
+        }
+    }
+
+    // body
+    sprite.cells = [];
+    sprite.container = game.add.sprite(0,0)
+    sprite.addChild(sprite.container)
+    style.fill = "#000000"
+    for (var i=0;i<3*10;i++) {
+        var cell = game.add.sprite(0,0)
+        cell.anchor.set(0.5, 0.5);
+        var celltext = game.add.text(0,0,"",style)
+        celltext.anchor.set(0.5, 0.4);
+        cell.addChild(celltext)
+        cell.text = celltext;
+        cell.visible = false;
+        sprite.container.addChild(cell);
+        sprite.cells.push(cell);
+    }
+    sprite.setData = (arr) => {
+        var i = 0,j = 0;
+        for (i=0;i<10&&i<arr.length;i++) {
+            for (j=0;j<3&&j<arr[i].length;j++) {
+                var cell = sprite.cells[i*3+j];
+
+                cell.text.setText(arr[i][j])
+                
+                cell.visible = true;
+
+                // layout
+                cell.x = (j * 2 + 1) * sprite.frameW / 6;
+                cell.y = (i + 0.5)*sprite.cellHeight 
+            }
+        }
+        for (i=arr.length;i<10;i++)
+            for (j=0;j<3;j++)
+                sprite.cells[i*3+j].visible = false;
+
+        sprite.container.y = 0.6*sprite.frameH - (arr.length*sprite.cellHeight)/2;
+    }
+
+    // layout
+    sprite.resizeFrame = (Width, Height) => {
+        sprite.frameW = Width;
+        sprite.frameH = Height;
+        // layout title
+        var titleHeight = Height * 0.2;
+        var titleWidth = Width / 3;
+        for (var i=0;i<3;i++) {
+            var text = sprite.title[i];
+            text.x = i * titleWidth;
+            text.y = 0;
+            text.setTextBounds(0,0,titleWidth,titleHeight)
+        }
+        // layout body
+        sprite.container.y = Height * 0.2;
+        var cellWidth = (Width/3)*0.8;
+        var cellHeight = cellWidth;
+        sprite.cellWidth = cellWidth;
+        sprite.cellHeight = cellHeight;
+        var graphics = game.add.graphics(0, 0);
+        graphics.lineStyle(3,0x5aedb9,1);
+        graphics.beginFill(0xffffff,1);
+        graphics.drawEllipse(0,0,cellWidth/2,cellHeight/2)
+        graphics.endFill();
+        var cellTexture = graphics.generateTexture();
+        
+        for (var i=0;i<3*10;i++) {
+            var cell = sprite.cells[i];
+            cell.setTexture(cellTexture)
+            //cell.text.setTextBounds(0,0,cellWidth,cellHeight);
+        }
+        graphics.destroy();
+
+    }
+
+    sprite.resizeFrame(width,height)
+    sprite.setTitle(['買入','價格','賣出'])
+    sprite.setData([]);
+    return sprite;
+}
+
 function getPriceCount() {
     var price,count;
     while(1) {
@@ -95,20 +197,17 @@ module.exports = function(game) {
             console.log('[state] auction')
         },
         create : function() {
-            //this.test = cell(game, 100, 30, 300, 300);
-            //this.test.text.setText('wow')
             var cellh = (game.height*game.resolution*0.8)/10;
-            this.title = table(game,game.width/2, game.height*game.resolution*0.1-cellh*0.5, 0,300,cellh,1);
-            this.table = table(game, game.width/2, game.height*game.resolution/2, 10, 300, cellh, 10);
-            this.title.setData([['買','價錢','賣']])
-
+            this.machine = auctionMachine(game, 0.3*game.width*game.resolution,0.1*game.height*game.resolution,0.4*game.width*game.resolution,0.6*game.height*game.resolution)
+            //this.machine.setData([['0','1','2'],[4,5,6],[7,8,9]])
+            
             testCA.onChange.add(function(list) {
-                console.log(list)
+                
                 var usearr = [];
                 list.reverse().forEach(data=>{
                     usearr.push([data.buyTotal, data.price,data.sellTotal])
                 })
-                this.table.setData(usearr);
+                this.machine.setData(usearr);
             },this);
             testCA.onResult.add(function(price) {
                 alert(`本次成交價為 ${price}`);
@@ -143,7 +242,10 @@ module.exports = function(game) {
 
         },
         render : function() {
-            game.debug.inputInfo(32,32);
+            //game.debug.inputInfo(32,32);
+            //game.debug.spriteBounds(this.machine.cells[0])
+            //game.debug.spriteBounds(this.machine.cells[0].text, 'rgba(255,255,255,0.6)')
+            //game.debug.spriteInfo(this.title.children[0].children[0].text,32,128)
         }
     };
 }
