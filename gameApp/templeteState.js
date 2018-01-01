79abde37be0435a287b6ea31b4c2fcf40b2a2a72
e24033150 <ann750028@gmail.com>
2018-01-01 15:20:24 +0800

    display player's information

--- gameApp/templeteState.js
+++ gameApp/templeteState.js
@@ -6,7 +6,6 @@ module.exports = function(game) {
         },
         create : function() {
             game.stage.backgroundColor = "ffffff";
-			var rectangles = game.add.group();
             // draw floor
             var floor = game.add.graphics(0, 0);
 			floor.lineStyle(10,0x000000,1);
@@ -19,17 +18,14 @@ module.exports = function(game) {
             this.machine.setData([[10,10,10]])
 			
 			this.walk = require('./walk')(game);
-			var player = this.walk.add_one_man(game,'playerwalk',0,game.height*0.45,game.height*0.6,100,1,100,200);
-			rectangles.add(player._stock_rect);
-			rectangles.add(player._money_rect);
+			var player = this.walk.add_one_man(game,'playerwalk',0,game.height*0.45,game.height*0.6,100,1,100,10);
 			player._sprite.animations.add('player_walk',[ 0,1,2,3,4,5,6,7,8], 8, true,true);
 			player._sprite.animations.play('player_walk');
-			var stupid = this.walk.add_one_man(game,'stupidwalk',game.width,game.height*0.45,game.height*0.6,100,-1,100,200);
-			rectangles.add(stupid._stock_rect);
-			rectangles.add(stupid._money_rect);
+			var stupid = this.walk.add_one_man(game,'stupidwalk',game.width,game.height*0.45,game.height*0.6,100,-1,100,10);
 			stupid._sprite.animations.add('stupid_walk',[ 0,1,2,3,4,5,6,7,8], 8, true,true);
 			stupid._sprite.animations.play('stupid_walk');
 			this.walk.two_people_walk_in(player,stupid);
+			var player_information = this.walk.display_information(player,window.innerWidth*0.15);
 			
         },
         update : function() {
--- gameApp/walk.js
+++ gameApp/walk.js
@@ -4,9 +4,9 @@ module.exports = function (game) {
 	//Note: (x,y) is at the sprite's center (because the anchor is set to 0.5)
 	walk.add_one_man = function(game,sprite,x,y,height,rect_width,mirror,money,stock) {
 		var man = {
-			_sprite: game.add.sprite(x, y, sprite),
 			_money_rect: game.add.graphics(0, 0),
 			_stock_rect: game.add.graphics(0, 0),
+			_sprite: game.add.sprite(x, y, sprite),
 			_floor: game.add.graphics(x,y+height*0.5),
 			_money : money,
 			_stock : stock,
@@ -26,11 +26,11 @@ module.exports = function (game) {
 								this._stock_rect.clear();
 								this._stock_rect.lineStyle(1,0x000000,1);
 								this._stock_rect.beginFill(0xf4b443,1);
-								this._stock_rect.drawRoundedRect(0,0,this._rect_width,this._height*stock*0.002,10);
+								this._stock_rect.drawRoundedRect(0,0,this._rect_width,this._height*stock*0.02,10);
 								this._stock_rect.endFill();
 								this._stock_rect.alignTo(this._floor, Phaser.TOP_RIGHT);
 								return;
-							},
+							}
 		}
 		man._sprite.anchor.setTo(0.5,0.5);
 
@@ -38,6 +38,7 @@ module.exports = function (game) {
 		h = man._sprite.height;
 		man._sprite.height = height*window.devicePixelRatio;
 		man._sprite.width = mirror*man._sprite.height*w/h;
+		man._sprite.frame = 9;
 		
 		man._money_rect.lineStyle(1,0x000000,1);
 		man._money_rect.beginFill(0xf4e643,1);
@@ -45,7 +46,7 @@ module.exports = function (game) {
 		man._money_rect.endFill();
 		man._stock_rect.lineStyle(1,0x000000,1);
 		man._stock_rect.beginFill(0xf4b443,1);
-		man._stock_rect.drawRoundedRect(0,0,rect_width,height*stock*0.002,10);
+		man._stock_rect.drawRoundedRect(0,0,rect_width,height*stock*0.02,10);
 		man._stock_rect.endFill();
 		man._floor.lineStyle(1,0x000000,0);
 		man._floor.beginFill(0x000000,0);
@@ -59,6 +60,7 @@ module.exports = function (game) {
 	};
 	
 	walk.two_people_walk_in = function(man1,man2) {
+		var information;
 		man1._sprite.animations.add('man1_walk_in',[ 0,1,2,3,4,5,6,7,8], 8, true,true);
 		man1._sprite.animations.play('man1_walk_in');
 		man2._sprite.animations.add('man2_walk_in',[ 0,1,2,3,4,5,6,7,8], 8, true,true);
@@ -127,7 +129,7 @@ module.exports = function (game) {
 		triangle.drawTriangle([ new Phaser.Point(ellipse.x, ellipse.y), new Phaser.Point(ellipse.x, ellipse.y+game.height*0.05), new Phaser.Point(man._sprite.x+man._mirror*game.width*0.07, man._sprite.y+game.height*0.06) ]);
 		triangle.endFill();
 		
-		var style = { font: "24px Arial", fill: "#ffffff", wordWrap: true, wordWrapWidth: ellipse.width, align: "center"};
+		var style = { font: "24px Microsoft JhengHei", fill: "#ffffff", wordWrap: true, wordWrapWidth: ellipse.width, align: "center"};
 		var text = game.add.text(ellipse.x, ellipse.y , content, style);
 		text.anchor.set(0.5);
 		setTimeout(function () {
@@ -137,71 +139,18 @@ module.exports = function (game) {
 		}, time)
 	};
 	
-	return walk;
+	walk.display_information = function(man,x){
+		var rect = game.add.graphics(x-man._rect_width,man._sprite.y-man._height*0.5);
+		rect.lineStyle(1,0xffffff,1);
+		rect.beginFill(0xffffff,1);
+		rect.drawRoundedRect(0,0,man._rect_width*2,100,10);
+		rect.endFill();
+		var style = { font: "24px Microsoft JhengHei", fill: "#000000", wordWrap: true, wordWrapWidth: rect.width, align: "left"};
+		var information = game.add.text(x, man._sprite.y-man._height*0.5 , '所剩金額 '+man._money+ '\n'+'擁有股票 '+man._stock, style);
+		information.anchor.set(0.5);
+		return information;
+	};
 	
-        /*
-		preload : function() {
-            console.log('[state] walk')
-        },
-		
-        create : function() {
-            
-			var button1 = game.add.graphics(game.width*0.5,game.height*0.01);
-			button1.beginFill(0xfffafa,0.5);
-			button1.lineStyle(2, 0x483D8B, 1);
-			button1.drawRoundedRect(0, 0, game.width*0.1, game.height*0.1,7);
-			button1.endFill();
-			button1.inputEnabled = true;
-			button1.input.useHandCursor = true;
-				
-			var button2 = game.add.graphics(game.width*0.61,game.height*0.01);
-			button2.beginFill(0xfffafa,0.5);
-			button2.lineStyle(2, 0x483D8B, 1);
-			button2.drawRoundedRect(0, 0, game.width*0.1, game.height*0.1,7);
-			button2.endFill();
-			button2.inputEnabled = true;
-			button2.input.useHandCursor = true;
-				
-			var button3 = game.add.graphics(game.width*0.72,game.height*0.01);
-			button3.beginFill(0xfffafa,0.5);
-			button3.lineStyle(2, 0x483D8B, 1);
-			button3.drawRoundedRect(0, 0, game.width*0.1, game.height*0.1,7);
-			button3.endFill();
-			button3.inputEnabled = true;
-			button3.input.useHandCursor = true;
-			
-			var button4 = game.add.graphics(game.width*0.83,game.height*0.01);
-			button4.beginFill(0xfffafa,0.5);
-			button4.lineStyle(2, 0x483D8B, 1);
-			button4.drawRoundedRect(0, 0, game.width*0.1, game.height*0.1,7);
-			button4.endFill();
-			button4.inputEnabled = true;
-			button4.input.useHandCursor = true;
-				
-			var style = { font: "18px 微軟正黑體", fill: "#000000",  align: "center"};
-			var button1text = game.add.text(button1.x + button1.width/2,button1.y + button1.height/2,"來回走",style);
-			button1text.anchor.set(0.5);
-			var button2text = game.add.text(button2.x + button2.width/2,button2.y + button2.height/2,"說話",style);
-			button2text.anchor.set(0.5);
-			var button3text = game.add.text(button3.x + button3.width/2,button3.y + button3.height/2,"金錢變化",style);
-			button3text.anchor.set(0.5);
-			var button4text = game.add.text(button4.x + button4.width/2,button4.y + button4.height/2,"股票變化",style);
-			button4text.anchor.set(0.5);
-				
-			button1.events.onInputDown.add(function(){
-				walk_around(stupid,window.innerWidth*0.95,window.innerWidth*0.85);
-			}, this);
-			button2.events.onInputDown.add(function(){
-				say(stupid,'我賣20!',3000);
-			}, this);
-			button3.events.onInputDown.add(function(){
-				stupid.change_money(Math.random() * (500 - 50) + 50);
-			}, this);
-			button4.events.onInputDown.add(function(){
-				stupid.change_stock(Math.random() * (500 - 50) + 50);
-			}, this);
-			
-        }
-		*/
+	return walk;    
 		
 }
