c16ece1a542ac35e4126f09a41560f97c850cc30
e24033150 <ann750028@gmail.com>
2018-01-07 16:00:43 +0800

    modify start and introduction state

--- gameApp/start.js
+++ gameApp/start.js
@@ -3,8 +3,8 @@ var button_music;
 function draw_button(){
 	var style = { font:"24px 微軟正黑體 " , fill: "#ffffff",  align: "center"};
 	butt =  {
-		_rect : game.add.graphics(game.world.centerX-75,game.world.centerY+10),
-		_text : game.add.text(game.world.centerX, game.world.centerY+40 , '開始', style)
+		_rect : game.add.graphics(game.world.centerX-75,game.world.centerY+70),
+		_text : game.add.text(game.world.centerX, game.world.centerY+100 , '開始', style)
 	};
 	
 	butt._rect.anchor.set(0.5);
@@ -24,14 +24,14 @@ function draw_button(){
 }
 function Out(but){
 	but.x = game.world.centerX-75;
-	but.y = game.world.centerY+10;
+	but.y = game.world.centerY+70;
 	but.scale.setTo(1, 1);
 	butt._text.scale.x = 1;
 	butt._text.scale.y = 1;
 }
 function Over(but){
 	but.x = game.world.centerX-77;
-	but.y = game.world.centerY+8;
+	but.y = game.world.centerY+68;
 	but.scale.setTo(1.05, 1.05);
 	butt._text.scale.x = 1.05;
 	butt._text.scale.y = 1.05;
@@ -40,7 +40,7 @@ function Down(but){
 	button_music.play();
 	but.clear();
 	but.x = game.world.centerX-73;
-	but.y = game.world.centerY+12;
+	but.y = game.world.centerY+72;
 	but.scale.setTo(0.95, 0.95);
 	butt._text.scale.x = 0.95;
 	butt._text.scale.y = 0.95;
@@ -72,7 +72,11 @@ module.exports = function(game) {
 			
 			var bc = game.add.sprite(game.width/2, game.height/2, 'startanimate');
 			bc.anchor.set(0.5,0.5)
-			bc.resizeFrame(bc,game.width *2,game.height *2)
+			w = bc.width;
+			h = bc.height;
+			bc.height = game.height*window.devicePixelRatio;
+			bc.width = bc.height*w/h;
+			//bc.resizeFrame(bc,game.width *2,game.height *2)
 			var bc1 = bc.animations.add('circle',[ 0,1,2,3,4,5,6,7,8,9,10,11], 8, true,true);
 			bc1.play('circle');
 			
@@ -84,7 +88,7 @@ module.exports = function(game) {
 				item.anchor.setTo(0.5,0.5);
 
 				// Add a simple bounce tween to each character's position.
-				game.add.tween(item).to({y: game.world.centerY-100}, 2400, Phaser.Easing.Bounce.Out, true, 1000 + 400 * i, 0);
+				game.add.tween(item).to({y: game.world.centerY-50}, 2400, Phaser.Easing.Bounce.Out, true, 1000 + 400 * i, 0);
 
 				// Add another rotation tween to the same character.
 				if(i!=4){
@@ -97,15 +101,6 @@ module.exports = function(game) {
 			}
 			tween.onComplete.add(draw_button, this);
 
-			/*遊戲背景動畫
-			var backgroundc = game.add.sprite(game.width/2,game.height/2,'start');
-            backgroundc.anchor.set(0.5,0.5)
-			backgroundc.resizeFrame(backgroundc,game.width * 2,game.height * 2)
-			backgroundc.frameName = '3'
-			*/
-
-			
-			
 			
         },
         update : function() {
