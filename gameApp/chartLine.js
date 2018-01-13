4f3435b825eca301a813ad5dbcd2e0af13969125
dada <e94031194@gmail.com>
2018-01-14 01:15:02 +0800

    [bye bye html button]

--- gameApp/chartLine.js
+++ gameApp/chartLine.js
@@ -1,34 +1,57 @@
-
 var chartLine;
-module.exports = function(game) {
-    return {
-        preload : function() {
-           console.log('[state] chartLine')
-        },
-        create : function() {
-            this.chartLine = game.add.sprite(50, 50);
-            chartLine = this.chartLine;
-            var width = 600;
-            var height = 600;
-            // 预留给轴线的距离
-            var padding = { top: 10, right: 10, bottom: 10, left: 10 };
-            //dataset
-            var maxNum = 60;  
-            var minNum = 50;
-            dataset = new Array();
-            var totalPoints = 12;//顯示幾筆
-            var i=1; 
-            var key = 1;
-            console.log(key)
-            var pausekey = function(){
-                if (key==0) {key=1;}
-                else {key=0;}
-            }
+var butt;
+function draw_button(){
+    var style = { font:"24px 微軟正黑體 " , fill: "#ffffff",  align: "center"};
+    butt =  {
+        _rect : game.add.graphics(game.world.centerX-75,game.world.centerY+70),
+        _text : game.add.text(game.world.centerX, game.world.centerY+100 , '開始', style)
+    };
+    
+    butt._rect.anchor.set(0.5);
+    butt._text.anchor.set(0.5);
+    butt._text.alpha = 0.1;
+    butt._rect.lineStyle(2,0x000000,1);
+    butt._rect.beginFill(0x5aedb9,1);
+    butt._rect.drawRoundedRect(0, 0, 150, 60,20);
+    butt._rect.endFill();
+    butt._rect.alpha = 0.1;
+    game.add.tween(butt._text).to( { alpha: 1 }, 500, "Linear", true);
+    game.add.tween(butt._rect).to( { alpha: 1 }, 500, "Linear", true);
+    butt._rect.inputEnabled = true;
+    butt._rect.events.onInputOut.add(Out, this);
+    butt._rect.events.onInputOver.add(Over, this);
+    butt._rect.events.onInputDown.add(Down, this);
+}
+function Out(but){
+    but.x = game.world.centerX-75;
+    but.y = game.world.centerY+70;
+    but.scale.setTo(1, 1);
+    butt._text.scale.x = 1;
+    butt._text.scale.y = 1;
+}
+function Over(but){
+    but.x = game.world.centerX-77;
+    but.y = game.world.centerY+68;
+    but.scale.setTo(1.05, 1.05);
+    butt._text.scale.x = 1.05;
+    butt._text.scale.y = 1.05;
+}
+function Down(but){
+    but.clear();
+    but.x = game.world.centerX-73;
+    but.y = game.world.centerY+72;
+    but.scale.setTo(0.95, 0.95);
+    butt._text.scale.x = 0.95;
+    butt._text.scale.y = 0.95;
+    but.lineStyle(3,0x000000,1);
+    but.beginFill(0x17ab76,1);
+    but.drawRoundedRect(0, 0, 150, 60,20);
+    but.endFill();
+    game.time.events.repeat(Phaser.Timer.SECOND * 0.01, 50, plotchartline, this);
+    
+}
+ var plotchartline = function(){
 
-            var plotchartline = function(){
-
-
-                   
             dataset.shift(); 
                 while (dataset.length < totalPoints) { 
                 var y = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum ;
@@ -139,13 +162,41 @@ module.exports = function(game) {
 
             
             }
-//按按鈕
+   var width = 600;
+            var height = 600;
+            // 预留给轴线的距离
+            var padding = { top: 10, right: 10, bottom: 10, left: 10 };
+            //dataset
+            var maxNum = 60;  
+            var minNum = 50;
+            dataset = new Array();
+            var totalPoints = 12;//顯示幾筆
+            var i=1; 
+            var key = 1;
+            console.log(key)
+            var pausekey = function(){
+                if (key==0) {key=1;}
+                else {key=0;}
+            }
 
+
+
+module.exports = function(game) {
+    return {
+        preload : function() {
+           console.log('[state] chartLine')
+        },
+        create : function() {
+            this.chartLine = game.add.sprite(50, 50);
+            chartLine = this.chartLine;
+         
+           
+//按按鈕
+            draw_button();
             d3.select("#save").on("click", () => {   
-            //pausekey();  
-            //console.log(key)   
-            //if (key==1) {
-            game.time.events.repeat(Phaser.Timer.SECOND * 0.01, 1000, plotchartline, this);
+              
+          
+           
             // }
             console.log(i)
         });
@@ -155,8 +206,11 @@ module.exports = function(game) {
             }
             console.log(i)
         });*/
-             
-        },
+                     
+
+
+
+        }, //end create
         update : function(){
      
         },
